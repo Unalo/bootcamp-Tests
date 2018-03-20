582cd3c3e835a5becb0d34767e09a0987e4f6236
unalo <unalomtshokotsha@gmail.com>
2018-03-20 10:04:28 +0200

    from where

--- test/fromWhere.test.js
+++ test/fromWhere.test.js
@@ -1,5 +1,11 @@
 describe('The fromWhere function' , function(){
-    it('takes a car registration number as a parameter and returns the town the car is from.' , function(){
+    it('takes a car registration number from Cape Town.' , function(){
       assert.equal(fromWhere("CA"),('Cape Town'));
     });
+      it('takes a car registration number from Bellville' , function(){
+        assert.equal(fromWhere("CY"),('Bellville'));
+        });
+        it('takes a car registration number from Paarl.' , function(){
+          assert.equal(fromWhere("CJ"),('Paarl'));
+          });
 });
--- test/greet.test.js
+++ test/greet.test.js
@@ -2,5 +2,4 @@ describe('The greet function' , function(){
     it('should greet Unalo properly' , function(){
       assert.equal(greet('Unalo'),'Hello Unalo');
     });
-
 });
